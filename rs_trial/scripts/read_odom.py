ab73039efd5472150bb3fb641885c0716d13ff8d
Miroslav Radojevic <miroslav.radojevic@gmail.com>
2020-08-17 09:26:30 +0200

    Odometry readings

--- rs_trial/scripts/read_odom.py
+++ rs_trial/scripts/read_odom.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python
-
 import rospy
 import time
 import sys
@@ -43,7 +42,7 @@ def callback_number(msg):
             msg.poses[-1].pose.position.y, \
             msg.poses[-1].pose.position.z)
 
-    with open(myargv[1].replace("/", "_") + "_" + tstamp + ".csv", "a") as f:
+    with open(myargv[1].replace("/", "_") + ".csv", "a") as f: # "_" + tstamp +
         f.write(log_line + "\n")
 
     rospy.loginfo(msg._type)
@@ -54,8 +53,8 @@ if __name__ == '__main__':
 
     if len(myargv) != 2:
         print("Invalid number of arguments.\n" +
-              "Usage: rosrun rs_trial read_odom.py method\n" +
-              "method: RTABMAP, ORB2, FLOAM")
+              "Usage: rosrun rs_trial " + __file__ + " method\n" +
+              "method: RTABMAP, ORB2, FLOAM...")
         sys.exit()
 
     rospy.init_node('read_odom', anonymous=True)
@@ -67,11 +66,11 @@ if __name__ == '__main__':
     if myargv[1] == "TF":
         sub = rospy.Subscriber("/tf", TFMessage, callback_number)
     elif myargv[1] == "RTABMAP":
-        sub = rospy.Subscriber("/odometry/filtered", Odometry, callback_number)
+        sub = rospy.Subscriber("/rtabmap/odom", Odometry, callback_number)
     elif myargv[1] == "ORB2":
         sub = rospy.Subscriber("/orb_slam2_rgbd/pose", PoseStamped, callback_number)
     elif myargv[1] == "FLOAM":
-        sub = rospy.Subscriber("/aft_mapped/trajectory", Path, callback_number)
+        sub = rospy.Subscriber("/odom", Odometry, callback_number)
     elif myargv[1] == "ALOAM":
         sub = rospy.Subscriber("/aft_mapped_path", Path, callback_number)
     elif myargv[1] == "ALOAM1":
@@ -86,6 +85,8 @@ if __name__ == '__main__':
         sub = rospy.Subscriber("/laser_odom_to_init", Odometry, callback_number)
     elif myargv[1] == "LOAM2":
         sub = rospy.Subscriber("/integrated_to_init", Odometry, callback_number)
+    elif myargv[1] == "UKF":
+        sub = rospy.Subscriber("/odometry/filtered", Odometry, callback_number)
     else:
         rospy.loginfo("Method " + myargv[1] + " not recognized.")
         sys.exit()
